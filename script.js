dabf87aec7b9aa6ee5befeaa1b46b9be787edefc
Andre <aa.andremacedo@gmail.com>
2022-02-16 16:19:03 -0300

    adiciona a solução para o desafio 7

--- script.js
+++ script.js
@@ -1,49 +1,42 @@
-
-
 const btnAdd = document.getElementById('criar-tarefa');
 btnAdd.addEventListener('click', function () {
   let inputTextField = document.getElementById('texto-tarefa');
   let taskList = document.querySelector('#lista-tarefas');
-  
-  if (inputTextField.value.length > 0) {  
+
+  if (inputTextField.value.length > 0) {
     let newLi = document.createElement('li');
     newLi.innerText = inputTextField.value;
+    newLi.className = 'itemList;'
     taskList.appendChild(newLi);
     inputTextField.value = '';
+    addBackGroundColor();
   } else {
     alert('Error: Digite ao menos 1 caractere.');
   }
-  
+
 });
 
 
 
-// const listItens = document.getElementsByClassName('li')
+let liItens = document.getElementsByTagName('li');
 
-// for(let i in listItens){
-//   listItens[i].addEventListener('click', function (event) {
-//     let inputTextField = document.getElementById('texto-tarefa');
-//     let taskList = document.querySelector('#lista-tarefas');
-    
-//     if (inputTextField.value.length > 0) {  
-//       let newLi = document.createElement('li');
-//       newLi.innerText = inputTextField.value;
-//       taskList.appendChild(newLi);
-//       inputTextField.value = '';
-//     } else {
-//       alert('Error: Digite ao menos 1 caractere.');
-//     }
-    
-//   });
-
-// }
+function addBackGroundColor(){
+  for (let i =0; i < liItens.length; i++){
+    console.log(liItens[i]);
+    liItens[i].addEventListener('click', function(event){
+      // liItens[i].className = 'item-todo';
+      liItens[i].style.backgroundColor = 'gray';
+    });
+  
+  }
+}
 
 
 
 function taskAdd() {
   let taskList = document.getElementById('lista-tarefas');
-  
-  
+
+
   // console.log(taskList);
 
 
